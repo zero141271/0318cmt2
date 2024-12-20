018cde639640c38c28362cd8324ea16eb13bbd9b
yinlin <yinlin@csdn.net>
2024-12-20 15:12:42 +0800

    update: 更新文件 DataProviderTest.cs  dasfsafsafsafsdf
    Signed-off-by: yinlin <yinlin@csdn.net>

--- DataProviderTest.cs
+++ DataProviderTest.cs
@@ -1,4 +1,4 @@
-﻿// The MIT License(MIT)
+// The MIT License(MIT)
 //
 // Copyright(c) 2021 Alberto Rodriguez Orozco & LiveCharts Contributors
 //
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-// 3333333333
+// 
 using System;
 using System.Linq;
 using LiveChartsCore.Defaults;
